c864ef7b7f8aa6ad7b3bc84c0a4892ea5ded20a6
Aashik Thapa <aashikthapa560@gmail.com>
2024-12-09 12:37:27 +0545

    nothing

--- Basic_python/function.py
+++ Basic_python/function.py
@@ -62,5 +62,71 @@ print(x)
 def adddd(c,d=0):
     result=c+d
     return result
-result=adddd(1)
-print(result)
+resultaa=adddd(1)
+print(resultaa)
+
+
+
+
+# local and global variable
+y=20
+def my_f():
+    x=10
+    print(x,y)
+my_f()
+
+
+z=2
+
+def my_global():
+    global z
+    z=3
+    print(z)
+
+my_global()
+print(z)
+
+
+
+def rev(word):
+    temp=word
+    rev=0
+    while temp>0:
+        digit=temp%10
+        rev=rev*10+digit
+        temp=temp//10
+    return rev
+
+def palindorm(word):
+    if(type(word)==str and word==word[: :-1]):
+        print("Palindorme")
+    elif(type(word)==int and word==rev(word)):
+        print("palindrom")
+    else:
+        print("not palindrom")
+palindorm("aa")
+palindorm(11011)
+
+def armstrong(number):
+    sum=0
+    temp=number
+    while temp>0:
+        digit=temp%10
+        sum=sum+digit**2
+        temp=temp//10
+armstrong(153)
+
+
+def palindorom(word):
+    if(type(word)==str and word==word[: :-1]):
+        print("Palindorme")
+    elif(type(word)==int):
+        ra=str(word)
+        if(ra==ra[::-1]):
+          print("pm")
+    else:
+        print("not palindrom")
+        
+palindorm("aa")
+palindorm(11011)
+palindorom(123)
